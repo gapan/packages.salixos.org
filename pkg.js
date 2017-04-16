b53b52e7e4ae19ad415b89f1b56f34a66379e7f1
George Vlahavas <vlahavas~at~gmail~dot~com>
2017-04-16 19:13:19 +0300

    Add showSlide function

--- pkg.js
+++ pkg.js
@@ -1,35 +1,47 @@
 var pkgSearch = function() {
     terms = document.getElementById("search").value;
-    version = document.getElementById("ver").value;
-    arch = document.getElementById("arch").value;
+    version = document.getElementById("search-ver").value;
+    arch = document.getElementById("search-arch").value;
     console.log("Searching for: " + terms);
     console.log("Version: " + version);
     console.log("Architecture: " + arch);
+
 }
 
 /* Executed when clicking the Browse button
  * on the main browse page */
 var pkgBrowse = function() {
-    let fout = document.getElementById("slide-browse-main");
-    let fin = document.getElementById("slide-browse-repo");
-    fout.setAttribute("style", "display: none");
-    fin.setAttribute("style", "display: block");
+    showSlide("slide-browse-repo");
 }
 
+/* Executed when selecting a repository
+ * to browse */
 var browseRepo = function() {
-    console.log("Browse repo");
+    showSlide("slide-browse-location");
 }
 
+/* Executed when selecting a location
+ * to browse */
 var browseLocation = function() {
-    console.log("Browse location");
+    showSlide("slide-browse-package");
 }
 
 var browsePkg = function(pkgname, pkgver, pkgrel) {
     arch = document.getElementById("search-arch").value;
     console.log("Browse package: " + pkgname + "-" + pkgver +
                     "-" + arch + "-" + pkgrel);
+    showSlide("slide-details");
 }
 
 var viewDep = function(dep) {
     console.log("Viewing dep: " + dep);
+}
+
+var showSlide = function(slide) {
+    let slides = document.getElementsByClassName("slide");
+    // first hide all slides
+    for (let i = 0; i < slides.length; i++)
+        document.getElementById(slides[i].id).setAttribute("style", "display: none");
+    // then show the one we want
+    document.getElementById(slide).setAttribute("style", "display: block");
 }
