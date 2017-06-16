cea67733ca99a1d98332f2fbe3edbd15918295c6
George Vlahavas <vlahavas@gmail.com>
2017-06-17 02:06:31 +0300

    Add Action class

---
+++ src/Action.js
@@ -0,0 +1,54 @@
+
+class Action {
+
+    constructor(screen) {
+        this.screen = screen;
+    }
+
+    pkgSearch() {
+        let terms = document.getElementById("search").value;
+        let version = document.getElementById("search-ver").value;
+        let arch = document.getElementById("search-arch").value;
+        console.log("Searching for: " + terms);
+        console.log("Version: " + version);
+        console.log("Architecture: " + arch);
+        this.screen.showSlide("slide-search-results");
+        this.screen.scrollToId('screen-lower');
+    }
+
+    /* Executed when clicking the Browse button
+    * on the main browse page */
+    pkgBrowse() {
+        this.screen.showSlide("slide-browse-repo");
+    }
+
+    /* Executed when selecting a repository
+    * to browse */
+    browseRepo() {
+        this.screen.showSlide("slide-browse-location");
+    }
+
+    /* Executed when selecting a location
+    * to browse */
+    browseLocation() {
+        this.screen.showSlide("slide-browse-package");
+    }
+
+    browsePkg(pkgname, pkgver, pkgrel) {
+        let arch = document.getElementById("browse-arch").value;
+        console.log("Browse package: " + pkgname + "-" + pkgver +
+                        "-" + arch + "-" + pkgrel);
+        this.screen.showSlide("slide-browse-details");
+    }
+
+    browseSearchedPkg(pkgname, pkgver, pkgrel) {
+        let arch = document.getElementById("search-arch").value;
+        console.log("Browse package: " + pkgname + "-" + pkgver +
+                        "-" + arch + "-" + pkgrel);
+        this.screen.showSlide("slide-search-details");
+    }
+
+    viewDep(dep) {
+        console.log("Viewing dep: " + dep);
+    }
+}
